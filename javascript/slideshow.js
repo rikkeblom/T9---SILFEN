65d1f924dccad2bad47e465c7f8ce1c0feab3e49
Justina Jemeljanovaite <just1528@stud.kea.dk>
2021-05-03 22:09:14 +0300

    slightly fixed desktop view on product page

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,16 +1,24 @@
 //source https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_rr
 
-let imageIndex = 0;
-carousel();
+window.addEventListener("resize", applyCarousel);
+window.addEventListener("load", applyCarousel);
 
-function carousel() {
-  let i;
-  const productImage = document.querySelectorAll('.product-photo');
-  for (i = 0; i < productImage.length; i++) {
-    productImage[i].style.display = 'none';  
-  }
-  imageIndex++;
-  if (imageIndex > productImage.length) {imageIndex = 1}    
-  productImage[imageIndex-1].style.display = 'block';  
-  setTimeout(carousel, 4000); // Change image every 4 seconds
+const mediaScreen = window.matchMedia('(max-width: 769px)');
+function applyCarousel() {
+    if (mediaScreen.matches) {
+        let imageIndex = 0;
+        carousel();
+        
+        function carousel() {
+          let i;
+          const productImage = document.querySelectorAll('.product-photo');
+          for (i = 0; i < productImage.length; i++) {
+            productImage[i].style.display = 'none';  
+          }
+          imageIndex++;
+          if (imageIndex > productImage.length) {imageIndex = 1}    
+          productImage[imageIndex-1].style.display = 'block';  
+          setTimeout(carousel, 4000); // Change image every 4 seconds
+        }
+    }
 }
