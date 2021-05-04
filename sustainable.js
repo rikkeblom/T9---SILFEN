229cc6f86ba3d99d1fa2af743b860f45bc7f9634
gintare kruminaite <gint0217@stud.kea.dk>
2021-05-04 10:40:33 +0200

    show more sustainabilty works

--- sustainable.js
+++ sustainable.js
@@ -1,14 +1,15 @@
-$(document).ready(function () {
-  $("#toggle").click(function () {
-    var elem = $("#toggle").text();
-    if (elem == "Read More") {
-      //Stuff to do when btn is in the read more state
-      $("#toggle").text("Read Less");
-      $("#text").slideDown();
-    } else {
-      //Stuff to do when btn is in the read less state
-      $("#toggle").text("Read More");
-      $("#text").slideUp();
-    }
-  });
-});
+function myFunction() {
+  var dots = document.querySelector(".dots");
+  var moreText = document.querySelector(".more");
+  var btnText = document.querySelector(".myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
